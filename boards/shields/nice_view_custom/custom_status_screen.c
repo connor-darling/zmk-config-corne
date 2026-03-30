ef566c32e5f996f3dccced7c984921b95aad47e8
Connor Darling <connordarling@me.com>
2026-03-30 00:12:27 -0700

    Refactor custom status screen function to create a new screen object instead of using the active display screen

--- boards/shields/nice_view_custom/custom_status_screen.c
+++ boards/shields/nice_view_custom/custom_status_screen.c
@@ -1,8 +1,8 @@
 #include <lvgl.h>
 #include <zmk/display.h>
 
-lv_obj_t *zmk_display_status_screen_custom(lv_disp_t *disp) {
-    lv_obj_t *screen = lv_disp_get_scr_act(disp);
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
 
     lv_obj_t *label = lv_label_create(screen);
     lv_label_set_text(label, "HELLO");
