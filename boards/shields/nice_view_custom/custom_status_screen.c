8aab3c42ca3c66eeb4b0565f85ef7fca5d155128
Connor Darling <connordarling@me.com>
2026-03-29 23:25:25 -0700

    Enhance custom status screen to conditionally include BLE status widget based on split role configuration

--- boards/shields/nice_view_custom/custom_status_screen.c
+++ boards/shields/nice_view_custom/custom_status_screen.c
@@ -3,19 +3,27 @@
 #include <zmk/display.h>
 
 #include "widgets/battery_status.h"
-#include "widgets/ble_status.h"
 #include "widgets/art.h"
 
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+#include "widgets/ble_status.h"
+#endif
+
 static struct zmk_widget_battery_status battery_status_widget;
-static struct zmk_widget_ble_status ble_status_widget;
 static struct zmk_widget_art art_widget;
+
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+static struct zmk_widget_ble_status ble_status_widget;
+#endif
 
 lv_obj_t *zmk_display_status_screen_custom(lv_disp_t *disp) {
     lv_obj_t *screen = lv_disp_get_scr_act(disp);
 
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     zmk_widget_ble_status_init(&ble_status_widget, screen);
     lv_obj_align(zmk_widget_ble_status_obj(&ble_status_widget),
                  LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
     zmk_widget_battery_status_init(&battery_status_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget),
